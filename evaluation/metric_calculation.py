corrected sAUC calculation

evaluation/metric_calculation.py
@@ -7,14 +7,18 @@ import os
 import numpy as np
 import pickle
 import datetime
+import torch
+from PIL import Image
 
 gt_directory = "/imatge/lpanagiotis/work/DHF1K/maps"
 #sm_directory = "/imatge/lpanagiotis/work/DHF1K/SGplus_predictions"
 #sm_directory = "/imatge/lpanagiotis/work/DHF1K/SG_predictions"
 sm_directory = "/imatge/lpanagiotis/work/DHF1K/SGtuned_predictions"
-
+RESCALE_GTs = False
 print("Now evaluating on {}".format(sm_directory))
 continue_calculations = False
+
+NUMBER_OF_VIDEOS = 700
 
 if continue_calculations:
     with open('metrics.txt', 'rb') as handle:
@@ -23,41 +27,75 @@ else:
     final_metric_list = []
 
 
+def sAUC_sampler(video_number, M=50):
+    # A sampler for the shuffled AUC metric. From the ground truths sample images at random; then aggregate them to feed into sAUC.
+
+    videos = list(range(1,NUMBER_OF_VIDEOS+1))
+    videos.remove(video_number) # Remove the video being evaluated.
+    video_sample_inds = np.random.choice(videos, M, replace=False)
+
+    for i in range(M):
+
+        video_sample = os.path.join(gt_directory, str(video_sample_inds[i]))
+        frames = os.listdir(video_sample)
+        frame_sample = np.random.choice(frames, 1, replace=False)[0]
+
+        sample_img = os.path.join(video_sample, frame_sample)
+        sample_img = np.asarray(Image.open(sample_img), dtype=np.float32)
+
+        if i == 0:
+            avg_img = sample_img
+        else:
+            avg_img = avg_img + sample_img
+
+    avg_img = np.asarray(avg_img/M, dtype=np.uint8)
+    avg_img = ((avg_img-avg_img.min())/(avg_img.max()-avg_img.min()))*255 #Rescale
+
+    return avg_img
+
 # The directories are named 1-1000 so it should be easy to iterate over them
-def inner_worker(i, packed, gt_path, sm_path): #packed should be a list of tuples (annotation, prediction)
+def inner_worker(n, sAUC_extramap, packed, gt_path, sm_path): #packed should be a list of tuples (annotation, prediction)
 
     gt, sm = packed
     ground_truth = cv2.imread(os.path.join(gt_path, gt),cv2.IMREAD_GRAYSCALE)
     saliency_map = cv2.imread(os.path.join(sm_path, sm),cv2.IMREAD_GRAYSCALE)
 
-    #print(np.max(ground_truth))
-    ground_truth = cv2.resize(ground_truth, (saliency_map.shape[1], saliency_map.shape[0]), interpolation=cv2.INTER_AREA)
-    # some error seems to occur, particularly on the first 3 metrics after the resize. CC and SIM are calculated normally. Noticeably the maximum value of 255 changes for the ground truth. It makes sense that this confuses location based metrics that aim to compare fixation points (hence maximum value locations).
 
-    ground_truth[ground_truth==np.max(ground_truth)]=255
-    # Rescaling turned out to cause a mess to the distribution, which results in a mess for the distribution based metrics (CC , SIM). This is evident from the fact that before rescaling the mean is close to 9 but afterwards it's close to 0. It is apparent that rescaling saliency maps down and then up again causes issues and should be avoided.
-    #ground_truth = (ground_truth-np.min(ground_truth))/(np.max(ground_truth)-np.min(ground_truth))
-    #ground_truth = ground_truth*255
+    if RESCALE_GTs:
+        # Avoid doing this in the future. GTs should not be manipulated.
+        #print(np.max(ground_truth))
+        ground_truth = cv2.resize(ground_truth, (saliency_map.shape[1], saliency_map.shape[0]), interpolation=cv2.INTER_AREA)
+        # some error seems to occur, particularly on the first 3 metrics after the resize. CC and SIM are calculated normally. Noticeably the maximum value of 255 changes for the ground truth. It makes sense that this confuses location based metrics that aim to compare fixation points (hence maximum value locations).
+
+        ground_truth[ground_truth==np.max(ground_truth)]=255
+        # Rescaling turned out to cause a mess to the distribution, which results in a mess for the distribution based metrics (CC , SIM). This is evident from the fact that before rescaling the mean is close to 9 but afterwards it's close to 0. It is apparent that rescaling saliency maps down and then up again causes issues and should be avoided.
+        #ground_truth = (ground_truth-np.min(ground_truth))/(np.max(ground_truth)-np.min(ground_truth))
+        #ground_truth = ground_truth*255
+    else:
+        saliency_map = cv2.resize(saliency_map, (ground_truth.shape[1], ground_truth.shape[0]), interpolation=cv2.INTER_LINEAR)
 
     saliency_map_norm = normalize_map(saliency_map) # The functions are a bit haphazard. Some have normalization within and some do not.
 
-    # Rescaling seems to fix AUC behavior, but CC and SIM break and give very low values. Weird behavior..
+    AUC_SHUF = auc_shuff(saliency_map_norm, ground_truth, other_map = ground_truth)
+    return AUC_SHUF
+    """
     # Calculate metrics
     AUC_JUDD = auc_judd(saliency_map_norm, ground_truth)
-    AUC_SHUF = auc_shuff(saliency_map_norm, ground_truth, ground_truth)
+    AUC_SHUF = auc_shuff(saliency_map_norm, ground_truth, sAUC_extramap)
     # the other ones have normalization within:
     NSS = nss(saliency_map, ground_truth)
     CC = cc(saliency_map, ground_truth)
     SIM = similarity(saliency_map, ground_truth)
 
     return ( AUC_JUDD,
-                    AUC_SHUF,
-                    NSS,
-                    CC,
-                    SIM )
+             AUC_SHUF,
+             NSS,
+             CC,
+             SIM )
+    """
 
 start = datetime.datetime.now().replace(microsecond=0)
-for i in range(1,701):
+for i in range(1, NUMBER_OF_VIDEOS+1):
 
     if i == 57: #Some unknown error occurs at this file, skip it
         continue
@@ -66,16 +104,19 @@ for i in range(1,701):
 
     gt_files = os.listdir(gt_path)
     sm_files = os.listdir(sm_path)
+
+    video_length = len(gt_files)
     #Now to sort based on their file number. The "key" parameter in sorted is a function based on which the sorting will happen (I use split to exclude the jpg/png from the).
     gt_files_sorted = sorted(gt_files, key = lambda x: int(x.split(".")[0]) )
     sm_files_sorted = sorted(sm_files, key = lambda x: int(x.split(".")[0]) )
     pack = zip(gt_files_sorted, sm_files_sorted)
     print("Files related to video {} sorted.".format(i))
 
+    sAUC_extramap = sAUC_sampler(video_number=i)
     """
     #Uncomment this segment if you want to debug something, so as to avoid parallel calculations and exit at 5 iterations.
     for n, packed in enumerate(pack):
-        a = inner_worker(n, packed, gt_path, sm_path)
+        a = inner_worker(n, sAUC_extramap, packed=packed, gt_path=gt_path, sm_path=sm_path)
         print(a)
         if n==5:
             exit()
@@ -83,8 +124,17 @@ for i in range(1,701):
 
     ##https://stackoverflow.com/questions/35663498/how-do-i-return-a-matrix-with-joblib-python
     from joblib import Parallel, delayed
-    metric_list = Parallel(n_jobs=8)(delayed(inner_worker)(n, packed, gt_path, sm_path) for n, packed in enumerate(pack)) #run 8 frames simultaneously
+    metric_list = Parallel(n_jobs=8)(delayed(inner_worker)(n, sAUC_extramap , packed=packed, gt_path=gt_path, sm_path=sm_path) for n, packed in enumerate(pack)) #run 8 frames simultaneously
 
+    aucs_mean = np.mean(metric_list)
+    print("AUC-SHUFFLED is {}".format(aucs_mean))
+    print("Time elapsed so far: {}".format(datetime.datetime.now().replace(microsecond=0)-start))
+    final_metric_list.append(aucs_mean)
+Aucs = np.mean(final_metric_list)
+print("Final average of metrics is:")
+print("AUC-SHUFFLED is {}".format(Aucs))
+
+"""
     aucj_mean = np.mean([x[0] for x in metric_list])
     aucs_mean = np.mean([x[1] for x in metric_list])
     nss_mean = np.mean([x[2] for x in metric_list])
@@ -121,4 +171,5 @@ print("AUC-SHUFFLED is {}".format(Aucs))
 print("NSS is {}".format(Nss))
 print("CC is {}".format(Cc))
 print("SIM is {}".format(Sim))
+"""
 

src/DHF1K_inference.py
@@ -20,12 +20,14 @@ clip_length = 10 #with 10 clips the loss seems to reach zero very fast
 number_of_videos = 700 # DHF1K offers 700 labeled videos, the other 300 are held back by the authors
 #pretrained_model = './SalGANplus.pt'
 #pretrained_model = '/imatge/lpanagiotis/work/SalGANmore/model_weights/gen_model.pt' # Vanilla SalGAN
-pretrained_model = './SalGAN.pt'
+#pretrained_model = './SalGAN.pt'
+pretrained_model = './SalGANmid.pt'
 frame_size = (192, 256)
 
 #dst = "/imatge/lpanagiotis/work/DHF1K/SGplus_predictions"
 #dst = "/imatge/lpanagiotis/work/DHF1K/SG_predictions"
-dst = "/imatge/lpanagiotis/work/DHF1K/SGtuned_predictions"
+#dst = "/imatge/lpanagiotis/work/DHF1K/SGtuned_predictions"
+dst = "/imatge/lpanagiotis/work/DHF1K/SGmid_predictions"
 # Parameters
 params = {'batch_size': 1, # number of videos / batch, I need to implement padding if I want to do more than 1, but with DataParallel it's quite messy
           'num_workers': 4,
@@ -67,6 +69,23 @@ def main():
 
         model.load_state_dict(checkpoint, strict=True)
         print("Pre-trained model SalGANplus loaded succesfully")
+
+        TEMPORAL = True
+
+    elif pretrained_model == './SalGANmid.pt':
+
+        model = SalGANmore.SalGANmid(seed_init=65, freeze=False)
+
+        temp = torch.load(pretrained_model)['state_dict']
+        # Because of dataparallel there is contradiction in the name of the keys so we need to remove part of the string in the keys:.
+        from collections import OrderedDict
+        checkpoint = OrderedDict()
+        for key in temp.keys():
+            new_key = key.replace("module.","")
+            checkpoint[new_key]=temp[key]
+
+        model.load_state_dict(checkpoint, strict=True)
+        print("Pre-trained model SalGANmid loaded succesfully")
 
         TEMPORAL = True
 

src/main.py
@@ -22,12 +22,12 @@ learning_rate = 0.000001 #
 decay_rate = 0.1
 momentum = 0.9
 weight_decay = 1e-4
-epochs = 5
+epochs = 6
 plot_every = 1
-pretrained_model = None #This refers to a pretrained model beyond SalGAN. In any case we are loading pretrained SalGAN weights.
-new_model = 'SalGANmid.pt'
+pretrained_model = None #This refers to a pretrained model beyond SalGAN. In default, we are loading the SalGAN weights that were pretrained on SALICON via adversarial training..
+new_model = 'SalGANdouble.pt'
 clip_length = 10
-number_of_videos = 4 # DHF1K offers 700 labeled videos, the other 300 are held back by the authors
+number_of_videos = 700 # DHF1K offers 700 labeled videos, the other 300 are held back by the authors
 
 TEMPORAL = True
 FREEZE = True
@@ -87,6 +87,9 @@ def main(params = params):
     elif new_model == 'SalGANmid.pt':
         model = SalGANmore.SalGANmid(seed_init=65, freeze=FREEZE)
         print("Initialized {}".format(new_model))
+    elif new_model == 'SalGANdouble.pt':
+        model = SalGANmore.SalGANdouble(seed_init=65, freeze=FREEZE)
+        print("Initialized {}".format(new_model))
     elif new_model == 'SalGAN.pt':
         model = SalGANmore.SalGAN()
         print("Initialized {}".format(new_model))
@@ -107,6 +110,9 @@ def main(params = params):
 
         elif new_model == 'SalGANmid.pt':
             optimizer = torch.optim.Adam([{'params': model.Gates.parameters()}], learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay)
+
+        elif new_model == 'SalGANdouble.pt':
+            optimizer = torch.optim.Adam([{'params': model.GatesE.parameters()}, {'params': model.GatesD.parameters()},{'params': model.final_convs.parameters()}], learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay)
 
     else:
         optimizer = torch.optim.Adam(model.parameters(), learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay)
@@ -135,6 +141,7 @@ def main(params = params):
     model = nn.DataParallel(model).cuda()
     cudnn.benchmark = True #https://discuss.pytorch.org/t/what-does-torch-backends-cudnn-benchmark-do/5936
     criterion = criterion.cuda()
+
     # =================================================
     # ================== Training =====================
 
@@ -252,6 +259,7 @@ def train(train_loader, model, criterion, optimizer, epoch, n_iter):
         start = datetime.datetime.now().replace(microsecond=0)
         print("Number of clips for video {} : {}".format(i,len(video)))
         state = None # Initially no hidden state
+        state_E, state_D = None, None
         for j, (clip, gtruths) in enumerate(video):
 
             n_iter+=j
@@ -274,7 +282,10 @@ def train(train_loader, model, criterion, optimizer, epoch, n_iter):
                     #print(clip[idx].size())
 
                     # Compute output
-                    state, saliency_map = model.forward(input_ = clip[idx], prev_state = state) # Based on the number of epoch the model will unfreeze deeper layers moving on to shallow ones
+                    if new_model == "SalGANdouble.pt":
+                        state_E, state_D, saliency_map = model.forward(input_ = clip[idx], prev_state = (state_E, state_D))
+                    else:
+                        state, saliency_map = model.forward(input_ = clip[idx], prev_state = state) # Based on the number of epoch the model will unfreeze deeper layers moving on to shallow ones
 
                     saliency_map = saliency_map.squeeze(0) # Target is 3 dimensional (grayscale image)
                     if saliency_map.size() != gtruths[idx].size():
@@ -304,7 +315,11 @@ def train(train_loader, model, criterion, optimizer, epoch, n_iter):
                 optimizer.step()
 
                 # Repackage to avoid backpropagating further through time
-                state = repackage_hidden(state)
+                if new_model == "SalGANdouble.pt":
+                    state_E = repackage_hidden(state_E)
+                    state_D = repackage_hidden(state_D)
+                else:
+                    state = repackage_hidden(state)
 
 
             else:
@@ -356,6 +371,7 @@ def validate(val_loader, model, criterion, epoch):
     for i, video in enumerate(val_loader):
         accumulated_losses = []
         state = None # Initially no hidden state
+        state_E, state_D = None, None
         for j, (clip, gtruths) in enumerate(video):
 
             clip = Variable(clip.type(dtype).transpose(0,1), requires_grad=False)
@@ -366,7 +382,10 @@ def validate(val_loader, model, criterion, epoch):
                 #print(clip[idx].size()) needs unsqueeze
                 # Compute output
                 if TEMPORAL:
-                    state, saliency_map = model.forward(clip[idx], state)
+                    if new_model == "SalGANdouble.pt":
+                        state_E, state_D, saliency_map = model.forward(clip[idx], (state_E, state_D))
+                    else:
+                        state, saliency_map = model.forward(clip[idx], state)
                 else:
                     saliency_map = model.forward(clip[idx])
 
@@ -380,7 +399,11 @@ def validate(val_loader, model, criterion, epoch):
                 loss = loss + criterion(saliency_map, gtruths[idx])
 
             if TEMPORAL:
-                state = repackage_hidden(state)
+                if new_model == "SalGANdouble.pt":
+                    state_E = repackage_hidden(state_E)
+                    state_D = repackage_hidden(state_D)
+                else:
+                    state = repackage_hidden(state)
 
             # Keep score
             accumulated_losses.append(loss.data)