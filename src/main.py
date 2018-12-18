02ecf86a556ebcd9b1305a489c3122c0b766eb99
Linardos <linardos.akis@gmail.com>
2018-12-18 17:08:03 +0100

    made a few vids

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ plot_every = 1
 pretrained_model = None #This refers to a pretrained model beyond SalGAN. In default, we are loading the SalGAN weights that were pretrained on SALICON via adversarial training..
 new_model = 'SalGANplus.pt'
 clip_length = 10
-number_of_videos = 5 # DHF1K offers 700 labeled videos, the other 300 are held back by the authors
+number_of_videos = 700 # DHF1K offers 700 labeled videos, the other 300 are held back by the authors
 
 TEMPORAL = True
 FREEZE = True
