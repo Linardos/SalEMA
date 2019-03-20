cbf170415e9869cc08c981e756fa40d54af9b0b4
Linardos <linardos.akis@gmail.com>
2019-03-20 16:55:09 +0100

    Added hollywood loader and inference

--- src/data_loader.py
+++ src/data_loader.py
@@ -129,6 +129,114 @@ class DHF1K_frames(data.Dataset):
 
         return packed
 
+# The DataLoader for our specific video datataset with extracted frames
+class Hollywood_frames(data.Dataset):
+
+  def __init__(self, clip_length, resolution=None, root_path = "~/work/Hollywood-2/testing/", load_gt = False):
+
+        self.cl = clip_length
+        self.root_path = root_path # in our case it's salgan saliency maps
+        self.ImageNet_mean = [103.939, 116.779, 123.68]
+        self.resolution = resolution
+        self.load_gt = load_gt
+        # A list to keep all video lists of salgan predictions, which will be our dataset.
+        self.video_list = []
+
+        # A list to keep all the dictionaries of ground truth - saliency map pairings for each video
+        self.gts_list = []
+
+        # A list to match an index to the video name
+        self.video_name_list = []
+
+        sample_list = os.listdir(root_path)
+        start = datetime.datetime.now().replace(microsecond=0) # Gives accurate human readable time, rounded down not to include too many decimals
+        count = 0
+        for i in sample_list:
+
+            frame_files = [os.path.join(self.root_path, str(i), "images", file) for file in  os.listdir(os.path.join(self.root_path, str(i), "images"))]
+            #print("for video {} the frames are {}".format(i, len(frame_files))) # This is correct
+
+            # Now to sort based on their file number. The "key" parameter in sorted is a function based on which the sorting will happen (I use split to exclude the jpg/png from the).
+            frame_files_sorted = sorted(frame_files)
+            # a list of lists
+            self.video_list.append(frame_files_sorted)
+            self.video_name_list.append(i)
+
+            count += 1
+            if load_gt:
+              # This is not correct, I never used the gt to train. Will need to change the code to load them.
+              gt_files = os.listdir(os.path.join(self.root_path, str(i), "maps"))
+              gt_files_sorted = sorted(gt_files)
+              pack = zip(gt_files_sorted, frame_files_sorted)
+
+              # Make dictionary where keys are the saliency maps and values are the ground truths
+              gt_frame_pairings = {}
+              for gt, frame in pack:
+                  gt_frame_pairings[frame] = gt
+
+              self.gts_list.append(gt_frame_pairings)
+
+            if count%50==0:
+              print("Video {} (Number {}) finished.".format(i, count))
+              print("Time elapsed so far: {}".format(datetime.datetime.now().replace(microsecond=0)-start))
+
+  def video_names(self):
+      return self.video_name_list
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.video_list)
+
+  def __getitem__(self, video_index):
+
+        'Generates one sample of data'
+        # Select sample video (frame list), in our case saliency map list
+        frames = self.video_list[video_index]
+        if self.load_gt:
+          gts = self.gts_list[video_index]
+
+
+        data = []
+        gt = []
+        packed = []
+        print(frames[0])
+        for i, path_to_frame in enumerate(frames):
+
+          X = cv2.imread(path_to_frame)
+          if self.resolution!=None:
+            X = cv2.resize(X, (self.resolution[1], self.resolution[0]), interpolation=cv2.INTER_AREA)
+          X = X.astype(np.float32)
+          X -= self.ImageNet_mean
+          #X = (X-np.min(X))/(np.max(X)-np.min(X))
+          X = torch.FloatTensor(X)
+          X = X.permute(2,0,1) # swap channel dimensions
+
+          data.append(X.unsqueeze(0))
+          # Load and preprocess ground truth (saliency maps)
+          if self.load_gt:
+
+            y = cv2.imread(path_to_frame, 0) # Load as grayscale
+            if self.resolution!=None:
+              y = cv2.resize(y, (self.resolution[1], self.resolution[0]), interpolation=cv2.INTER_AREA)
+            y = (y-np.min(y))/(np.max(y)-np.min(y))
+            y = torch.FloatTensor(y)
+
+            gt.append(y.unsqueeze(0))
+
+          if (i+1)%self.cl == 0 or i == (len(frames)-1):
+
+            data_tensor = torch.cat(data,0)
+            data = []
+            if self.load_gt:
+              gt_tensor = torch.cat(gt,0)
+              gt = []
+              packed.append((data_tensor,gt_tensor)) # pack a list of data with the corresponding list of ground truths
+            else:
+              packed.append((data_tensor, "_"))
+
+
+        return packed
+
 
 
 
--- src/inference.py
+++ src/inference.py
@@ -10,7 +10,7 @@ import torch.backends.cudnn as cudnn
 from torch import nn
 from torch.utils import data
 from torch.autograd import Variable
-from data_loader import DHF1K_frames, Ego_frames
+from data_loader import DHF1K_frames, Ego_frames, Hollywood_frames
 
 dtype = torch.FloatTensor
 if torch.cuda.is_available():
@@ -22,27 +22,30 @@ RESIDUAL,
 pretrained_model,
 dst
 """
-dataset_name = "DHF1K"
+dataset_name = "Hollywood-2"
 clip_length = 10 #with 10 clips the loss seems to reach zero very fast
 STARTING_VIDEO = 601
 NUMBER_OF_VIDEOS = 700# DHF1K offers 700 labeled videos, the other 300 are held back by the authors
-EMA_LOC = 7     # 30 is the bottleneck
+EMA_LOC = 30     # 30 is the bottleneck
 #EMA_LOC_2 = 54
 RESIDUAL = False
 DOUBLE = False
-DROPOUT = False
+DROPOUT = True
 ALPHA = 0.1
 #pretrained_model = '/imatge/lpanagiotis/work/SalGANmore/src/model_weights/gen_model.pt' # Vanilla SalGAN
 #pretrained_model = './SalGAN.pt'
 #pretrained_model = 'model_weights/salgan_salicon.pt' #JuanJo's weights, set EMA_LOC to None for original SalBCE, otherwise EMA will be added
 #pretrained_model = './SalGANplus.pt'
 #pretrained_model = './SalGANmid.pt'
-pretrained_model = './SalEMA{}D.pt'.format(EMA_LOC)
+pretrained_model = 'SalEMA{}D.pt'.format(EMA_LOC)
 #pretrained_model = 'SalEMA{}&{}.pt'.format(EMA_LOC,EMA_LOC_2)
 frame_size = (192, 256)
 # Destination for predictions:
-dst = "/imatge/lpanagiotis/work/{}/{}_test_set_predictions".format(dataset_name, pretrained_model.replace(".pt", ""))
+dst = "/imatge/lpanagiotis/work/{}/{}_predictions".format(dataset_name, pretrained_model.replace(".pt", ""))
+dst = "/home/linardos/Hollywood-2/testing"
 #dst = "/imatge/lpanagiotis/work/{}/SG_predictions".format(dataset_name)
+frames_path = "/imatge/lpanagiotis/work/DHF1K/frames"
+gt_path = "/imatge/lpanagiotis/work/DHF1K/maps"
 
 params = {'batch_size': 1,
           'num_workers': 4,
@@ -57,8 +60,8 @@ def main(dataset_name=dataset_name):
     if dataset_name == "DHF1K":
         print("Commencing inference for dataset {}".format(dataset_name))
         dataset = DHF1K_frames(
-            frames_path = "/imatge/lpanagiotis/work/DHF1K/frames",
-            gt_path = "/imatge/lpanagiotis/work/DHF1K/maps",
+            frames_path = frames_path,
+            gt_path = gt_path,
             starting_video = STARTING_VIDEO,
             number_of_videos = NUMBER_OF_VIDEOS,
             clip_length = clip_length,
@@ -69,10 +72,18 @@ def main(dataset_name=dataset_name):
     elif dataset_name == "Egomon":
         print("Commencing inference for dataset {}".format(dataset_name))
         dataset = Ego_frames(
-            frames_path = "/imatge/lpanagiotis/work/Egomon/frames",
+            frames_path = frames_path,
             clip_length = clip_length,
             resolution = frame_size)
         activity = dataset.match_i_to_act
+
+    elif dataset_name == "Hollywood-2":
+        print("Commencing inference for dataset {}".format(dataset_name))
+        dataset = Hollywood_frames(
+            root_path = dst,
+            clip_length = clip_length,
+            resolution = frame_size)
+        video_name_list = dataset.video_names() #match an index to the sample video name
 
     print("Size of test set is {}".format(len(dataset)))
 
@@ -181,8 +192,10 @@ def main(dataset_name=dataset_name):
         exit()
 
     #model = nn.DataParallel(model).cuda()
-    cudnn.benchmark = True #https://discuss.pytorch.org/t/what-does-torch-backends-cudnn-benchmark-do/5936
-    model = model.cuda()
+    if torch.cuda.is_available():
+        cudnn.benchmark = True #https://discuss.pytorch.org/t/what-does-torch-backends-cudnn-benchmark-do/5936
+        model = model.cuda()
+
     # ==================================================
     # ================== Inference =====================
 
@@ -236,6 +249,45 @@ def main(dataset_name=dataset_name):
                 if TEMPORAL:
                     state = repackage_hidden(state)
 
+        elif dataset_name == "Hollywood-2":
+
+            video_dst = os.path.join(dst, video_name_list[i], '{}_predictions'.format(pretrained_model.replace(".pt", "")))
+            print("Destination: {}".format(video_dst))
+            if not os.path.exists(video_dst):
+                os.mkdir(video_dst)
+
+            for j, (clip, _) in enumerate(video):
+                clip = Variable(clip.type(dtype).transpose(0,1), requires_grad=False)
+                if DOUBLE:
+                    if state == None:
+                        state = (None, None)
+                    for idx in range(clip.size()[0]):
+                        # Compute output
+                        state, saliency_map = model.forward(input_ = clip[idx], prev_state_1 = state[0], prev_state_2 = state[1])
+
+                        saliency_map = saliency_map.squeeze(0) # Target is 3 dimensional (grayscale image)
+
+                        post_process_saliency_map = (saliency_map-torch.min(saliency_map))/(torch.max(saliency_map)-torch.min(saliency_map))
+                        utils.save_image(post_process_saliency_map, os.path.join(video_dst, "{}.png".format(str(count).zfill(4))))
+
+                else:
+                    for idx in range(clip.size()[0]):
+                        # Compute output
+                        if TEMPORAL:
+                            state, saliency_map = model.forward(input_ = clip[idx], prev_state = state)
+                        else:
+                            saliency_map = model.forward(input_ = clip[idx])
+
+                        count+=1
+                        saliency_map = saliency_map.squeeze(0)
+
+                        post_process_saliency_map = (saliency_map-torch.min(saliency_map))/(torch.max(saliency_map)-torch.min(saliency_map))
+                        utils.save_image(post_process_saliency_map, os.path.join(video_dst, "{}{}.png".format(video_name_list[i][:-1], str(count).zfill(5))))
+                        if count == 1:
+                            print("The final destination is {}. Cancel now if this is incorrect".format(os.path.join(video_dst, "{}{}.png".format(video_name_list[i][:-1], str(count).zfill(5)))))
+
+                if TEMPORAL:
+                    state = repackage_hidden(state)
         elif dataset_name == "Egomon":
 
             video_dst = os.path.join(dst, activity[i])
