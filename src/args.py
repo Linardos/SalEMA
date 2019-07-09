b43bbb595f63825c35727690260432527e0cd30b
Akis Linardos <linardos.akis@gmail.com>
2019-07-09 14:10:08 +0100

    Update args.py

--- src/args.py
+++ src/args.py
@@ -4,7 +4,7 @@ def get_inference_parser():
 
     parser = argparse.ArgumentParser(description="Video saliency prediction. Use this script to get saliency maps for your videos.  If you're using your own dataset make sure to follow the same folder structure as DHF1K: Videos divided into numbered folders [0001, 0002 etc] that include their extracted frames [also numbered 0001.png, 0002.png etc].")
 
-    parser.add_argument('-use_gpu', dest='use_gpu', default=True, help="Boolean value, set to True if you are using CUDA.")
+    parser.add_argument('-use_gpu', dest='use_gpu', default=True, type=bool, help="Boolean value, set to True if you are using CUDA.")
     parser.add_argument('-dst', dest='dst', default="/imatge/lpanagiotis/work/DHF1K", help="Add root path to output predictions to.")
     parser.add_argument('-src', dest='src', default="/imatge/lpanagiotis/work/DHF1K", help="Add root path to your dataset.")
 
@@ -20,9 +20,9 @@ def get_inference_parser():
     # Args for EMA
     parser.add_argument('-alpha', dest='alpha', default=None, type=float, help='Input value for alpha.')
     parser.add_argument('-ema_loc', dest='ema_loc', default=30, type=int, help='Input number of layer to place EMA on')
-    parser.add_argument('-dropout', dest='dropout', default=True, help='Boolean value set to True if model includes dropout.')
-    parser.add_argument('-residual', dest='residual', default=False, help='Boolean value set to True if model includes residual connection on SalEMA')
-    parser.add_argument('-double_ema', dest='double_ema', default=False, help='Boolean value set to True to activate two EMAs simultaneously')
+    parser.add_argument('-dropout', dest='dropout', default=True, type=bool, help='Boolean value set to True if model includes dropout.')
+    parser.add_argument('-residual', dest='residual', default=False, type=bool, help='Boolean value set to True if model includes residual connection on SalEMA')
+    parser.add_argument('-double_ema', dest='double_ema', default=False, type=bool, help='Boolean value set to True to activate two EMAs simultaneously')
 
 
     return parser
@@ -54,8 +54,8 @@ def get_training_parser():
     # Args for EMA
     #parser.add_argument('-alpha', dest='alpha', default=None, help='Input value for alpha. Set to None in order for the model to learn alpha (recommended).')
     parser.add_argument('-ema_loc', dest='ema_loc', default=30, type=int, help='Input number of layer to place EMA on')
-    parser.add_argument('-dropout', dest='dropout', default=True, help='Boolean value set to True if model includes dropout.')
-    parser.add_argument('-residual', dest='residual', default=False, help='Boolean value set to True if model includes residual connection on SalEMA')
+    parser.add_argument('-dropout', dest='dropout', default=True, type=bool, help='Boolean value set to True if model includes dropout.')
+    parser.add_argument('-residual', dest='residual', default=False, type=bool, help='Boolean value set to True if model includes residual connection on SalEMA')
     parser.add_argument('-double_ema', dest='double_ema', default=False, help="Set to False if you're not using two EMAs. If you want to use2 EMAs, use the number upon which you want the second ema to be located at")
 
 
